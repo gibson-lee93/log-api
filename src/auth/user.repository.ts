8636697cb65bb31c95a5eb5b5469c5f1c0f9d2fb
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-08-30 16:04:38 +0900

    Added signup function

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-	
+	async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+		const { email, password } = authCredentialsDto;
+		const user = this.create({ email, password });
+		await this.save(user);
+	}
 }
