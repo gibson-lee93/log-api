50ccd2801b7ec5ff622c36df577292eb70c1b7f3
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-08-30 14:40:41 +0900

    TypeORM setting for DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'log-api',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
 })
 export class AppModule {}
