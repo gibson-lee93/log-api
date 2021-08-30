73566434dad56920663ff008c06dd43989e5bb73
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-08-30 16:21:06 +0900

    Added duplicate email error handling

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,22 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 	async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		const { email, password } = authCredentialsDto;
 		const user = this.create({ email, password });
-		await this.save(user);
+
+		try {
+			await this.save(user);
+		} catch (error) {
+			if(error.code === 'ER_DUP_ENTRY') { //duplicate email
+				throw new ConflictException('Email already exists');
+			} else {
+				throw new InternalServerErrorException();
+			}
+		}
 	}
 }
